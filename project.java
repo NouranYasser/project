d96770f214358fdc8503715258166ba522ab0dc9
menaa123 <120986917+menaa123@users.noreply.github.com>
2022-12-19 03:58:29 +0200

    Update project.java

--- project.java
+++ project.java
@@ -1,1 +1,16 @@
 
+package sleepingbr;
+
+public class project 
+{
+    public static void main(String[] args) 
+    {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new InputFrame().setVisible(true);
+            }
+        });
+    }
+    
+}
